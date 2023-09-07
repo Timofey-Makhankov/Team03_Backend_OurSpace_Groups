9bdc3d8e3a8b6e5dcb9edb769310071e9de814d8
Generaladvisor Lam <113605832+Generaladvisor-Lam@users.noreply.github.com>
2023-09-07 13:20:01 +0200

    configured GroupDTP.java

--- src/main/java/com/example/demo/domain/group/dto/GroupDTO.java
+++ src/main/java/com/example/demo/domain/group/dto/GroupDTO.java
@@ -1,7 +1,33 @@
 package com.example.demo.domain.group.dto;
 
 import com.example.demo.core.generic.AbstractDTO;
+import com.example.demo.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
 public class GroupDTO extends AbstractDTO {
 
+    private String name;
+    private String description;
+    private String motto;
+    private String logo;
+    private List<User> users;
+
+    public GroupDTO(UUID id, String name, String description, String motto, String logo, List<User> users) {
+        super(id);
+        this.name = name;
+        this.description = description;
+        this.motto = motto;
+        this.logo = logo;
+        this.users = users;
+    }
 }
